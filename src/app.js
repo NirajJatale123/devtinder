cbfc579c1d8b55b37e52b0da2f7e67bda4c3ab43
NirajJatale <Niraj.Jatale@asurion.com>
2024-10-05 21:54:51 +0530

    managing diff routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express=require('express');
 const app=express();
 
-app.use((req,res) => {
-    res.send("Hello");
+//app.use("/", (req,res) => {
+  //  res.send("Hello");
+//});
+
+app.use("/Hello", (req,res) => {
+    res.send("Hello Niraj");
 });
+
+app.use("/test", (req,res) => {
+    res.send("Hello route");
+});
+
 app.listen(3000);
 
 console.log("server is running on port 40000");
